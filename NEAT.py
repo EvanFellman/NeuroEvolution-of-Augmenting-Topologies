d30545d4b52dc8cd8aa00d560369278eeb3a2b88
evanfellman <evan.fellman@gmail.com>
2020-09-29 20:38:23 -0400

    TURBO BOOST

--- NEAT.py
+++ NEAT.py
@@ -62,16 +62,36 @@ class Generation:
                 c.mutate()
                 self.nns.append(c)
 
+class DependObj:
+    def __init__(self, nodeNum, dependsOn, allDependants):
+        self.nodeNum = nodeNum
+        self.dependsOn = dependsOn
+        self.allDependants = allDependants
+    def __str__(self):
+        return "<NodeNum: {}, dependsOn: {}, allDependants: {}>".format(self.nodeNum, [a.start for a in self.dependsOn], self.allDependants)
+    def __repr__(self):
+        return self.__str__()
 
+def largest(arr):
+    if len(arr) == 0:
+        return -1 * math.inf
+    else:
+        return max(arr)
 
 class NeuralNetwork:
     def __init__(self, inputSize, outputSize):
         self.edges = []
         self.inputs = list(range(inputSize))
         self.outputs = list(range(inputSize, inputSize + outputSize))
+        self.depend = [DependObj(i, [], []) for i in (self.inputs + self.outputs) ]
         for inputNode in self.inputs:
             for outputNode in self.outputs:
-                self.edges.append(Edge(inputNode, outputNode))
+                e = Edge(inputNode, outputNode)
+                self.edges.append(e)
+                self.depend[outputNode].dependsOn.append(e)
+                self.depend[outputNode].allDependants.append(inputNode)
+        #This is makes it go super speedy
+        self.depend.sort(key=lambda x: len(x.allDependants))
         self.highestNode = inputSize + outputSize - 1
 
     #NeuralNetwork.computeOutput(inputVector: number[]): number[]
@@ -79,35 +99,34 @@ class NeuralNetwork:
         nodeValues = {}
         for i in range(len(inputVector)):
             nodeValues[i] = inputVector[i]
-        flag = True
-        while flag:
-            i = len(inputVector) - 1
-            canCalc = False
-            while i in nodeValues.keys() or not canCalc:
-                canCalc = True
-                acc = 0
-                for e in [a for a in self.edges if a.end == i]:
-                    if e.start not in nodeValues.keys():
-                        canCalc = False
-                    else:
-                        acc += nodeValues[e.start] * e.weight
-                if i in nodeValues.keys() or not canCalc:
-                    i += 1
-            nodeValues[i] = acc
-            if nodeValues[i] < 0:
-                nodeValues[i] = 0
-            flag = False
-            for k in self.outputs:
-                if k not in nodeValues.keys():
-                    flag = True
+        for dependObj in self.depend:
+            if dependObj.nodeNum in self.inputs:
+                continue
+            acc = 0
+            for e in dependObj.dependsOn:
+                acc += nodeValues[e.start] * e.weight
+            nodeValues[dependObj.nodeNum] = acc
         return [nodeValues[i] for i in self.outputs]
 
     def addNode(self):
         self.highestNode += 1
         edgeIndex = int(math.floor(len(self.edges) * random.random()))
         edge = self.edges.pop(random.choice(range(len(self.edges))))#self.edges.pop(edgeIndex)
-        self.edges.append(Edge(edge.start, self.highestNode, edge.weight))
-        self.edges.append(Edge(self.highestNode, edge.end, 1))
+        newE1 = Edge(edge.start, self.highestNode, edge.weight)
+        newE2 = Edge(self.highestNode, edge.end, 1)
+        self.edges.append(newE1)
+        self.edges.append(newE2)
+        for dependObj in self.depend:
+            if dependObj.nodeNum == edge.end:
+                dependObj.nodeNum = edge.end
+                dependObj.dependsOn = [newE2] + [u for u in dependObj.dependsOn if u.start != edge.start]
+                dependObj.allDependants = [self.highestNode] + dependObj.allDependants 
+            elif edge.end in dependObj.allDependants:
+                dependObj.allDependants.append(self.highestNode)
+        for dependObj in self.depend:
+            if dependObj.nodeNum == newE1.start:
+                self.depend.append(DependObj(newE1.end, [newE1], [newE1.start] + dependObj.allDependants))
+        self.depend.sort(key=lambda x: len(x.allDependants))
 
     def addEdge(self):
         allPairs = []
@@ -119,17 +138,46 @@ class NeuralNetwork:
         for i in range(len(allPairs)):
             a, b = allPairs[i]
             remove = False
-            for e in self.edges:
-                if a == e.start and b == e.end:
-                    remove = True
-
+            if a in self.outputs or b in self.inputs:
+                remove = True
+            if not remove:
+                for dependObj in self.depend:
+                    if dependObj.nodeNum == a and b in dependObj.allDependants:
+                        remove = True
+                        break
+            if not remove:
+                for e in self.edges:
+                    if a == e.start and b == e.end:
+                        remove = True
+                        break
             if not remove:
                 allPairsAcc.append((a, b))
         if len(allPairsAcc) == 0:
             self.mutateEdge()
         else:
-            start, end = random.choice(allPairsAcc)#[math.floor(len(allPairsAcc) * random())]
-            self.edges.append(Edge(start, end))
+            start, end = random.choice(allPairsAcc)
+            e = Edge(start, end)
+            self.edges.append(e)
+            startAllDependants = []
+            for dependObj in self.depend:
+                if dependObj.nodeNum == start:
+                    startAllDependants = dependObj.allDependants
+                    break
+            for dependObj in self.depend:
+                if dependObj.nodeNum == e.end:
+                    for toAdd in startAllDependants:
+                        if toAdd not in dependObj.allDependants:
+                            dependObj.allDependants.append(toAdd)
+                    dependObj.allDependants.append(start)
+                    dependObj.dependsOn.append(e)
+                elif e.end in dependObj.allDependants:
+                    for toAdd in startAllDependants:
+                        if toAdd not in dependObj.allDependants:
+                            dependObj.allDependants.append(toAdd)
+                    dependObj.allDependants.append(start)
+            self.depend.sort(key=lambda x: len(x.allDependants))
+
+
 
     def mutateEdge(self):
         edgeIndex = int(math.floor(len(self.edges) * random.random()))
@@ -142,6 +190,9 @@ class NeuralNetwork:
         out.highestNode = self.highestNode
         out.inputs = self.inputs
         out.outputs = self.outputs
+        out.depend = [DependObj(a.nodeNum, [i.copy() for i in a.dependsOn], [i for i in a.allDependants]) for a in self.depend]
+        out.depend.sort(key=lambda x: len(x.allDependants))
+
         return out
 
     def mutate(self):
@@ -164,209 +215,6 @@ class Edge:
     #Edge.copy(void): Edge
     def copy(self):
         return Edge(self.start, self.end, self.weight)
-
-
-
-
-
-
-class TabQAgent(object):
-    """Tabular Q-learning agent for discrete state/action spaces."""
-
-    def __init__(self):
-        self.epsilon = 0.01 # chance of taking a random action instead of the best
-
-        self.logger = logging.getLogger(__name__)
-        if False: # True if you want to see more information
-            self.logger.setLevel(logging.DEBUG)
-        else:
-            self.logger.setLevel(logging.INFO)
-        self.logger.handlers = []
-        self.logger.addHandler(logging.StreamHandler(sys.stdout))
-
-        self.actions = ["movenorth 1", "movesouth 1", "movewest 1", "moveeast 1"]
-        self.q_table = {}
-        self.canvas = None
-        self.root = None
-
-    def updateQTable( self, reward, current_state ):
-        """Change q_table to reflect what we have learnt."""
-        
-        # retrieve the old action value from the Q-table (indexed by the previous state and the previous action)
-        old_q = self.q_table[self.prev_s][self.prev_a]
-        
-        # TODO: what should the new action value be?
-        new_q = old_q
-        
-        # assign the new action value to the Q-table
-        self.q_table[self.prev_s][self.prev_a] = new_q
-        
-    def updateQTableFromTerminatingState( self, reward ):
-        """Change q_table to reflect what we have learnt, after reaching a terminal state."""
-        
-        # retrieve the old action value from the Q-table (indexed by the previous state and the previous action)
-        old_q = self.q_table[self.prev_s][self.prev_a]
-        
-        # TODO: what should the new action value be?
-        new_q = old_q
-        
-        # assign the new action value to the Q-table
-        self.q_table[self.prev_s][self.prev_a] = new_q
-        
-    def act(self, world_state, agent_host, current_r ):
-        """take 1 action in response to the current world state"""
-        
-        obs_text = world_state.observations[-1].text
-        obs = json.loads(obs_text) # most recent observation
-        self.logger.debug(obs)
-        if not u'XPos' in obs or not u'ZPos' in obs:
-            self.logger.error("Incomplete observation received: %s" % obs_text)
-            return 0
-        current_s = "%d:%d" % (int(obs[u'XPos']), int(obs[u'ZPos']))
-        self.logger.debug("State: %s (x = %.2f, z = %.2f)" % (current_s, float(obs[u'XPos']), float(obs[u'ZPos'])))
-        if current_s not in self.q_table:
-            self.q_table[current_s] = ([0] * len(self.actions))
-
-        # update Q values
-        if self.prev_s is not None and self.prev_a is not None:
-            self.updateQTable( current_r, current_s )
-
-        self.drawQ( curr_x = int(obs[u'XPos']), curr_y = int(obs[u'ZPos']) )
-
-        # select the next action
-        rnd = random.random()
-        if rnd < self.epsilon:
-            a = random.randint(0, len(self.actions) - 1)
-            self.logger.info("Random action: %s" % self.actions[a])
-        else:
-            m = max(self.q_table[current_s])
-            self.logger.debug("Current values: %s" % ",".join(str(x) for x in self.q_table[current_s]))
-            l = list()
-            for x in range(0, len(self.actions)):
-                if self.q_table[current_s][x] == m:
-                    l.append(x)
-            y = random.randint(0, len(l)-1)
-            a = l[y]
-            self.logger.info("Taking q action: %s" % self.actions[a])
-
-        # try to send the selected action, only update prev_s if this succeeds
-        try:
-            agent_host.sendCommand(self.actions[a])
-            self.prev_s = current_s
-            self.prev_a = a
-
-        except RuntimeError as e:
-            self.logger.error("Failed to send command: %s" % e)
-
-        return current_r
-
-    def run(self, agent_host):
-        """run the agent on the world"""
-
-        total_reward = 0
-        
-        self.prev_s = None
-        self.prev_a = None
-        
-        is_first_action = True
-        
-        # main loop:
-        world_state = agent_host.getWorldState()
-        while world_state.is_mission_running:
-
-            current_r = 0
-            
-            if is_first_action:
-                # wait until have received a valid observation
-                while True:
-                    time.sleep(0.1)
-                    world_state = agent_host.getWorldState()
-                    for error in world_state.errors:
-                        self.logger.error("Error: %s" % error.text)
-                    for reward in world_state.rewards:
-                        current_r += reward.getValue()
-                    if world_state.is_mission_running and len(world_state.observations)>0 and not world_state.observations[-1].text=="{}":
-                        total_reward += self.act(world_state, agent_host, current_r)
-                        break
-                    if not world_state.is_mission_running:
-                        break
-                is_first_action = False
-            else:
-                # wait for non-zero reward
-                while world_state.is_mission_running and current_r == 0:
-                    time.sleep(0.1)
-                    world_state = agent_host.getWorldState()
-                    for error in world_state.errors:
-                        self.logger.error("Error: %s" % error.text)
-                    for reward in world_state.rewards:
-                        current_r += reward.getValue()
-                # allow time to stabilise after action
-                while True:
-                    time.sleep(0.1)
-                    world_state = agent_host.getWorldState()
-                    for error in world_state.errors:
-                        self.logger.error("Error: %s" % error.text)
-                    for reward in world_state.rewards:
-                        current_r += reward.getValue()
-                    if world_state.is_mission_running and len(world_state.observations)>0 and not world_state.observations[-1].text=="{}":
-                        total_reward += self.act(world_state, agent_host, current_r)
-                        break
-                    if not world_state.is_mission_running:
-                        break
-
-        # process final reward
-        self.logger.debug("Final reward: %d" % current_r)
-        total_reward += current_r
-
-        # update Q values
-        if self.prev_s is not None and self.prev_a is not None:
-            self.updateQTableFromTerminatingState( current_r )
-            
-        self.drawQ()
-    
-        return total_reward
-        
-    def drawQ( self, curr_x=None, curr_y=None ):
-        scale = 40
-        world_x = 6
-        world_y = 14
-        if self.canvas is None or self.root is None:
-            self.root = tk.Tk()
-            self.root.wm_title("Q-table")
-            self.canvas = tk.Canvas(self.root, width=world_x*scale, height=world_y*scale, borderwidth=0, highlightthickness=0, bg="black")
-            self.canvas.grid()
-            self.root.update()
-        self.canvas.delete("all")
-        action_inset = 0.1
-        action_radius = 0.1
-        curr_radius = 0.2
-        action_positions = [ ( 0.5, action_inset ), ( 0.5, 1-action_inset ), ( action_inset, 0.5 ), ( 1-action_inset, 0.5 ) ]
-        # (NSWE to match action order)
-        min_value = -20
-        max_value = 20
-        for x in range(world_x):
-            for y in range(world_y):
-                s = "%d:%d" % (x,y)
-                self.canvas.create_rectangle( x*scale, y*scale, (x+1)*scale, (y+1)*scale, outline="#fff", fill="#000")
-                for action in range(4):
-                    if not s in self.q_table:
-                        continue
-                    value = self.q_table[s][action]
-                    color = int( 255 * ( value - min_value ) / ( max_value - min_value )) # map value to 0-255
-                    color = max( min( color, 255 ), 0 ) # ensure within [0,255]
-                    color_string = '#%02x%02x%02x' % (255-color, color, 0)
-                    self.canvas.create_oval( (x + action_positions[action][0] - action_radius ) *scale,
-                                             (y + action_positions[action][1] - action_radius ) *scale,
-                                             (x + action_positions[action][0] + action_radius ) *scale,
-                                             (y + action_positions[action][1] + action_radius ) *scale, 
-                                             outline=color_string, fill=color_string )
-        if curr_x is not None and curr_y is not None:
-            self.canvas.create_oval( (curr_x + 0.5 - curr_radius ) * scale, 
-                                     (curr_y + 0.5 - curr_radius ) * scale, 
-                                     (curr_x + 0.5 + curr_radius ) * scale, 
-                                     (curr_y + 0.5 + curr_radius ) * scale, 
-                                     outline="#fff", fill="#fff" )
-        self.root.update()
 
 gen = Generation(80, inputSize=3, outputSize=3)
 fitnesses = [fitness(i) for i in gen.nns]
